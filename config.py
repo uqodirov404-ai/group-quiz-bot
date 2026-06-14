d92e86ec54e0f66ead9ae85ec7b25071faa196d7
uqodirov404-ai <uqodirov404@gmail.com>
2026-06-14 13:58:56 +0500

    Upload/update config.py

--- config.py
+++ config.py
@@ -1,8 +1,14 @@
 import os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 ADMIN_ID = int(os.environ.get("ADMIN_ID", "162634410"))
 GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY", "")
+NEON_DATABASE_URL = os.environ.get("NEON_DATABASE_URL", "")
 
-EXCEL_PATH = os.environ.get("EXCEL_PATH", "lib_6a104385a7508.xlsx")
-DB_PATH = os.environ.get("DB_PATH", "quiz_database.db")
+QUIZ_BOT_TOKEN = os.environ.get("QUIZ_BOT_TOKEN", "")
+QUIZ_DB_PATH = os.environ.get("QUIZ_DB_PATH", "quiz_database.db")
+QUIZ_EXCEL_PATH = os.environ.get("QUIZ_EXCEL_PATH", "lib_6a104385a7508.xlsx")
+
+ESSE_BOT_TOKEN = os.environ.get("ESSE_BOT_TOKEN", "")
+WEBAPP_URL_ESSE = os.environ.get("WEBAPP_URL_ESSE", "")
+
+SAVOLJAVOB_BOT_TOKEN = os.environ.get("SAVOLJAVOB_BOT_TOKEN", "")
