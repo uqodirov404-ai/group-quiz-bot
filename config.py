5416222fa6760fec532013bbfcf43cc1021d1d2a
uqodirov404-ai <uqodirov404@gmail.com>
2026-06-14 13:37:32 +0500

    Upload bot source files for server deployment

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8669114731:AAHZbDk7mc-on6ADUYww6b5iExHhNPmDIg4")
+ADMIN_ID = int(os.environ.get("ADMIN_ID", "162634410"))
+GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY", "AQ.Ab8RN6LRnSYREugJsm7sxyCcUH8RgLlRRfjyLFWLxj5_aMjzJA")
+
+EXCEL_PATH = r"C:\Users\hp\Downloads\lib_6a104385a7508.xlsx"
+DB_PATH = "quiz_database.db"
